eb9fbd0a00a42daa36348e63c683e6702ee07624
Everton Paiva <evertonpaiva@gmail.com>
2019-06-12 11:17:41 -0300

    Alterando o endereço da API no build de produção

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -35,7 +35,12 @@ plugins.push(
     )
 );
 
+let SERVICE_URL = JSON.stringify('http://localhost:3000');
+
 if(process.env.NODE_ENV == 'production') {
+
+
+    SERVICE_URL = JSON.stringify('http://endereco-da-sua-api');
 
     plugins.push(new webpack.optimize.ModuleConcatenationPlugin());
 
@@ -51,6 +56,10 @@ if(process.env.NODE_ENV == 'production') {
         canPrint: true
     }));
 }
+
+plugins.push(new webpack.DefinePlugin({
+    SERVICE_URL
+}));
 
 module.exports = {
     entry: {
